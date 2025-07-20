6ef1d05ea25b85b48ca5cb52ca62cb90100a6e72
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-07-20 17:50:02 +0700

    Update control_command.h

--- esp_command/control_command.h
+++ esp_command/control_command.h
@@ -21,7 +21,7 @@ extern sem_t *uart_sem;
 void Uart_Init(speed_t baudrate, char *device);
 void write_command(Command cmd);
 void write_init(uint32_t baudrate);
-unsigned char *Read_Response(int timeout_ms, uint16_t *bytes_read);
+unsigned char* Read_Response(uint32_t timeout_ms, uint16_t* bytes_read_out);
 speed_t map_to_speed(uint32_t baud_num);
 void save_config(uint32_t baud, const char *dev);
 uint8_t load_config(uint32_t *baud, char **dev);
