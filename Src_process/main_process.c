601c7e4d892f021770421efe2a8258a00e5c8567
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-10 23:20:41 +0700

    Update main_process.c

--- Src_process/main_process.c
+++ Src_process/main_process.c
@@ -31,11 +31,18 @@ void cleanup_on_exit(void) {
 // ==================== MAIN FUNCTION ====================
 int main(void) {
     atexit(cleanup_on_exit); // Register cleanup function to be called on exit
-
+    uint8_t load_try = 0;
     // Load node configuration FIRST
     if (load_nodes_config("../config.json") != 0) {
         fprintf(stderr, "Failed to load node configuration\n");
-        return -1;
+        load_try = 1;
+    }
+    // If first load failed, try fallback config
+    if(load_try == 1) {
+        if (load_nodes_config("nodes_config.json") != 0) {
+            fprintf(stderr, "Failed to load node configuration from fallback\n");
+            return -1;
+        }
     }
 
     // Get default values from config instead of hard-coding
