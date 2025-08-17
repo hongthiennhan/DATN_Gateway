82f95db0f6e9ca2f2507891a38807b637ef4591f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-17 22:57:05 +0700

    Update thread_func.c

--- Src_process/thread_func.c
+++ Src_process/thread_func.c
@@ -19,9 +19,9 @@ shared_data_t command_data = {
 
 // ==================== UART THREAD - Passive listening for node data ====================
 void *uart_thread_func(void *arg) {
-    #ifdef DEBUG
+    // #ifdef DEBUG
     fprintf(stderr, "UART thread started - listening for automatic node data\n");
-    #endif
+    // #endif
     
     uint16_t data_len = 0;
     unsigned char *data_buffer = NULL;
