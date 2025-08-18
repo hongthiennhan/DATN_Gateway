a3a751bf6079e79c53f40ae77bb00c0a28892b03
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-19 00:53:54 +0700

    Update main_process.c

--- Src_process/main_process.c
+++ Src_process/main_process.c
@@ -89,14 +89,14 @@ int main(void)
     pthread_t uart_thread;
     pthread_create(&uart_thread, NULL, uart_thread_func, NULL);
     // Start UI thread
-    // pthread_t ui_thread;
-    // pthread_create(&ui_thread, NULL, ui_thread_func, ui_args);
+    pthread_t ui_thread;
+    pthread_create(&ui_thread, NULL, ui_thread_func, ui_args);
     pthread_t mqtt_thread;
     pthread_create(&mqtt_thread, NULL, mqtt_thread_func, NULL);
 
     // Main waits for threads to finish (does nothing else)
     pthread_join(uart_thread, NULL);
-    // pthread_join(ui_thread, NULL);
+    pthread_join(ui_thread, NULL);
     pthread_join(mqtt_thread, NULL);
 
     // Cleanup the device string
