68541f4378ce64498de9286ec8615d70e9828ffd
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-18 11:55:34 +0700

    Update

--- uart_command/control_command.h
+++ uart_command/control_command.h
@@ -6,15 +6,12 @@
 // Remove hard-coded CONFIG_FILE - now get from config
 
 extern int uart_fd;
-extern sem_t *uart_sem;
 
 void Uart_Init(speed_t baudrate, char *device);
 void write_command(uint8_t cmd);
 void write_init(uint32_t baudrate);
 unsigned char* Read_Response(uint32_t timeout_ms, uint16_t* bytes_read_out);
 speed_t map_to_speed(uint32_t baud_num);
-void save_config(uint32_t baud, const char *dev);
-uint8_t load_config(uint32_t *baud, char **dev);
 void Clear_Startup_UART(int fd, uint32_t flush_duration_ms);
 extern int Check_UART_Data_Available(void);
 #endif // __CONTROL_COMMAND_H__
