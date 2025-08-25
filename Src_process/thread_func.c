bb2f1e65e6c77d0f0d4c5fd2f8061102125f16c3
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-25 21:13:36 +0700

    Update thread_func.c

--- Src_process/thread_func.c
+++ Src_process/thread_func.c
@@ -15,7 +15,7 @@ shared_data_t command_data = {
     .cond = PTHREAD_COND_INITIALIZER
 };
 thread_pause_t uart_pause = {
-    .is_paused = true, 
+    .is_paused = false, 
     .mutex = PTHREAD_MUTEX_INITIALIZER, 
     .cond = PTHREAD_COND_INITIALIZER
 };
