6cfc39947a3105837d16c183dc60941685e9fa97
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-25 20:27:22 +0700

    Update main_process.c

--- Src_process/main_process.c
+++ Src_process/main_process.c
@@ -100,10 +100,10 @@ int main(void)
 
     // Main waits for threads to finish (does nothing else)
     pthread_join(uart_thread, NULL);
+    pause_thread(&uart_pause);
     pthread_join(ui_thread, NULL);
     pthread_join(mqtt_thread, NULL);
     pthread_join(modbus_thread, NULL);
-    pause_thread(&uart_pause);
     // Cleanup the device string
     free(device);
     return 0;
