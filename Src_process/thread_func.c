0277311f640133f25e0172a468aaf3c9dde1f97c
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-25 21:38:14 +0700

    Update thread_func.c

--- Src_process/thread_func.c
+++ Src_process/thread_func.c
@@ -228,7 +228,7 @@ void *modbus_thread_func(void *arg) {
                         data_frame.function.write = command_buffer[0];
                         data_frame.data = command_buffer + 1;
 // #ifdef DEBUG
-                        printf("Modbus check node %d\n", node->node_id);
+                        printf("Modbus check node %d, command %s\n", node->node_id, node->detection_commands[0].command);
 // #endif
                         // Send detection command from JSON
                         Modbus_Write_Frame(&data_frame);
