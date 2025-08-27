6cc4e4449015f8ee60b443c76a42b5ee58699c24
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-27 15:26:09 +0700

    Update thread_func.c

--- Src_process/thread_func.c
+++ Src_process/thread_func.c
@@ -239,11 +239,12 @@ void *modbus_thread_func(void *arg) {
                         if (timeout_ms <= 0) timeout_ms = 1000; // Default 1 second
 
                         receive_frame = Modbus_Read_Response(timeout_ms);
-                        snprintf(receive_data, sizeof(receive_data), "Auto Data[%d bytes]: %s%s",
-                            receive_frame->frame_length, receive_frame->data, (receive_frame->frame_length > 50) ? "..." : "");
+
                         if (receive_frame) {
                             // Process the received frame
                             printf("Receive Data! \n");
+                            snprintf(receive_data, sizeof(receive_data), "Auto Data[%d bytes]: %s%s",
+                                     receive_frame->frame_length, receive_frame->data, (receive_frame->frame_length > 50) ? "..." : "");
                             update_mqtt_data_from_response(node, receive_frame->data, receive_frame->frame_length);
                             free(receive_frame);
                         }
