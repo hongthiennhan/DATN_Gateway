16eb030da5d21ff920c14541b8aac861cd878deb
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-08-30 19:47:01 +0700

    update

--- CAN_handler/CAN_handler.c
+++ CAN_handler/CAN_handler.c
@@ -1,4 +1,28 @@
 #include "CAN_handler.h"
 #include "main.h"
 
-// Code for CAN communication
+void CAN_Init(speed_t baudrate, char *device) {
+    // Initialization code for CAN
+}
+
+void CAN_Write_Data(uint8_t *data, size_t len) {
+    // Code to write data to CAN
+}
+
+unsigned char* CAN_Read_Response(uint32_t timeout_ms) {
+    // Code to read response from CAN
+    return NULL;
+}
+
+int Check_CAN_Data_Available(void) {
+    // Code to check if CAN data is available
+    return 0;
+}
+
+void CAN_USB_to_Byte(CAN_Message_USB *usb_msg, uint8_t *byte_array) {
+    // Code to convert USB message to byte array
+}
+
+void CAN_to_Byte(CAN_Message *can_msg, uint8_t *byte_array) {
+    // Code to convert CAN message to byte array
+}
--- CAN_handler/CAN_handler.h
+++ CAN_handler/CAN_handler.h
@@ -1,5 +1,30 @@
 #ifndef __CAN_HANDLER_H__
 #define __CAN_HANDLER_H__
 #include "main.h"
-//Code for CAN communication
+typedef struct {
+    uint16_t ID; // 11 bit identifier
+    uint8_t RTR; // 1 bit Remote Transmission Request
+    uint8_t IDE; // 1 bit Identifier Extension
+    uint8_t reserved; // 1 bits reserved
+    uint8_t DLC; // 4 bits Data Length Code
+    uint8_t* data; // 0 - 8 bytes of data
+    uint8_t data_len;
+} CAN_Message;
+
+typedef struct{
+    uint8_t header;
+    uint8_t command;
+    uint16_t id;
+    uint8_t *data;
+    uint8_t footer;
+    uint8_t data_len;
+} CAN_Message_USB;
+
+void CAN_Init(speed_t baudrate, char *device);
+void CAN_Write_Data(uint8_t *data, size_t len);
+unsigned char* CAN_Read_Response(uint32_t timeout_ms);
+extern int Check_CAN_Data_Available(void);
+void CAN_USB_to_Byte(CAN_Message_USB *usb_msg, uint8_t *byte_array);
+void CAN_to_Byte(CAN_Message *can_msg, uint8_t *byte_array);
+
 #endif
