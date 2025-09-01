5b76be8926b903c431e11a3e197eb081a523b2c0
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-09-01 11:59:58 +0700

    Update mqtt_thread.h

--- mqtt_thread/mqtt_thread.h
+++ mqtt_thread/mqtt_thread.h
@@ -8,7 +8,6 @@ void *mqtt_thread_func(void *arg);
 // Config-driven function declarations - FIXED: Use actual typedefs instead of struct forward declarations
 void update_mqtt_data_from_response(node_config_t *node, unsigned char *resp, uint16_t resp_len);
 void build_telemetry_payload(char *payload, size_t payload_size, time_t timestamp);
-int request_config_json_robust(void);
 char *get_local_ip(void);
 mqtt_config_t *safe_get_mqtt_config(void);
 #endif // MQTT_THREAD_H
